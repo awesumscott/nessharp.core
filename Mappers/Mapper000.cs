58b8497a61e11089a0e67a1dc744d6018b406abf
Scott Lowe <scott@scottlowe.us>
2020-09-15 03:34:40 -0400

    mapper 0 mirroring param

--- Mappers/Mapper000.cs
+++ Mappers/Mapper000.cs
@@ -5,12 +5,18 @@ using System.Text;
 namespace NESSharp.Core.Mappers {
 	public class Mapper000 : IMapper {
 		public int Number => 0;
+		private MirroringOptions _mirroring;
+		
+		public Mapper000(MirroringOptions mirroring = MirroringOptions.Vertical) {
+			_mirroring = mirroring;
+		}
 
 		public void Init(List<Bank> Prg, List<Bank> Chr, HeaderOptions headerOpts) {
 			Prg.Add(new Bank(MemorySizes.KB_32, 0x8000, true));
 			Chr.Add(new Bank(MemorySizes.KB_8, 0, true));
 			headerOpts.PrgRomBanks = 2;
 			headerOpts.ChrRomBanks = 1;
+			headerOpts.Mirroring = _mirroring;
 		}
 
 		public void WriteInterrupts(List<Bank> Prg, byte[] interrupts, Action<Bank, byte[]> writeFunc) {
